0d3830d2014f879a238f5ed7257c48e6be2defc2
Jonas <jonas.swift@gmx.com>
2016-02-01 07:26:02 +0800

    #DB config1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	secret: 'boozyanalytics_secret_salt',
-	pgDatabase: 'postgres://ixmpiybcyrdnoq:5l1cPIPR4eslmEIZmZ9xqKCsNb@ec2-204-236-226-63.compute-1.amazonaws.com:5432/drbaajn2jc217',//'postgres://Developer:abc@localhost:5432/boozy_stats',
+	pgDatabase: 'postgres://hdcjgdqavhiewe:Al2aCvkZIo-Qd-UjFlt8GYAfD9@ec2-107-20-242-191.compute-1.amazonaws.com:5432/dbsbud2fhknldi',//'postgres://Developer:abc@localhost:5432/boozy_stats',
 	passcode: 'bz_0098_3150',
 	adminPassword: 'admin',
 	rootURL: 'https://boozyanalytics.herokuapp.com' //'http://localhost:8080'
